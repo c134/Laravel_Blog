a1b46b0884be5421d6e5a74491e147134e9584b3
qr1 <crt782@gmail.com>
2015-09-14 01:03:22 +0300

    Added:  show, store methods

--- app/Http/Controllers/ArticlesController.php
+++ app/Http/Controllers/ArticlesController.php
@@ -3,15 +3,29 @@
 namespace App\Http\Controllers;
 
 use App\Article;
-use Illuminate\Http\Request;
+//use Illuminate\Http\Request;
 
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Input;
+use Request;
 
 class ArticlesController extends Controller
 {
     public function create()
     {
-        return view('articles.create',compact('article'));
+        return view('articles.create');
+    }
+
+    public function store()
+    {
+        $input = Request::all();
+        Article::create($input);
+        return redirect('articles');
+    }
+
+    public function show(Article $article)
+    {
+        return view('articles.show', compact('article'));
     }
 }
