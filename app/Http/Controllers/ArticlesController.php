1aca30b468b00c89292df5b0ab57430c8a1587e6
qr1 <crt782@gmail.com>
2015-09-14 00:04:09 +0300

    Added:  create method

---
+++ app/Http/Controllers/ArticlesController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Article;
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+
+class ArticlesController extends Controller
+{
+    public function create()
+    {
+        return view('articles.create',compact('article'));
+    }
+}
