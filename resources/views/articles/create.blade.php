6814068dd07e2faa600717b63d272ae2d4a9f622
qr1 <crt782@gmail.com>
2015-09-14 22:17:43 +0300

    Added array to from open, files set to true

--- resources/views/articles/create.blade.php
+++ resources/views/articles/create.blade.php
@@ -2,7 +2,7 @@
 @section('content')
     <h1>Write a new Article</h1>
     <hr/>
-    {!! Form::open(['url'=>'articles']) !!}
+    {!! Form::open(array('url'=>'articles', 'files' => true)) !!}
     @include('articles.form',['submitButtonText' => 'Add Article'])
     {!! Form::close() !!}
     @include('errors.list')
