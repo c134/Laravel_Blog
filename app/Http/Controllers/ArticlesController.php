ca02fc289a21d2729af05d466a1e13f5355ebd60
qr1 <crt782@gmail.com>
2015-09-16 20:32:28 +0300

    Added destroy method

--- app/Http/Controllers/ArticlesController.php
+++ app/Http/Controllers/ArticlesController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Article;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 use App\Http\Requests\ArticleRequest;
 use App\Http\Requests;
@@ -31,14 +32,18 @@ class ArticlesController extends Controller
             $imgTitle = $request->title;
             $imagePath = 'uploads/' . $imgTitle . '.jpg';
             $request->image_path = $imagePath;
-
+            $request->published_at = Carbon::now();
             Article::create(array('title' => $request->title,
                 'body' => $request->body,
-                'image_path' => $imagePath));
+                'image_path' => $imagePath, 'published_at' => $request->published_at));
 
             Image::make($file)->resize(300, 200)->save($imagePath);
-        }
-        else{
+
+            return redirect('articles')->with([
+                'flash_message' => 'Your article has been created',
+                'flash_message_important' => true
+            ]);
+        } else {
             $url = 'https://ajax.googleapis.com/ajax/services/search/images?v=1.0&rsz=1&q=' . $request->title;
             $url = file_get_contents($url);
             $file = json_decode($url);
@@ -46,21 +51,45 @@ class ArticlesController extends Controller
             $imgTitle = $request->title;
             $imagePath = 'uploads/' . $imgTitle . '.jpg';
             $request->image_path = $imagePath;
-
+            $request->published_at = Carbon::now();
             Article::create(array('title' => $request->title,
                 'body' => $request->body,
-                'image_path' => $imagePath));
+                'image_path' => $imagePath, 'published_at' => $request->published_at));
 
             Image::make($file)->resize(300, 200)->save($imagePath);
+
+            return redirect('articles')->with([
+                'flash_message' => 'Your article has been created',
+                'flash_message_important' => true
+            ]);
         }
 
     }
-
 
     public function show(Article $article)
     {
         return view('articles.show', compact('article'));
     }
+
+    public function edit(Article $article)
+    {
+        return view('articles.edit', compact('article'));
+    }
+
+    public function update(Article $article, ArticleRequest $request)
+    {
+        $article->update($request->all());
+        return redirect('articles');
+    }
+
+    public function destroy($id) {
+
+        $article = Article::findOrFail($id);
+        $article->delete();
+        return redirect('articles');
+    }
+
+
 
 
 }
