7676e3b10965435ca0621e13dd7af14e35b6e3fa
qr1 <crt782@gmail.com>
2015-09-16 16:23:09 +0300

    Added delete form

--- resources/views/articles/edit.blade.php
+++ resources/views/articles/edit.blade.php
@@ -7,5 +7,11 @@
 
     {!! Form::close() !!}
 
+    {!! Form::open(['action' => ['ArticlesController@destroy', $article->id], 'method' => 'DELETE']) !!}
+    {!! Form::submit('Delete', ['class'=> 'btn btn-danger']) !!}
+    {!! Form::close() !!}
+    <a href="{{action('ArticlesController@index')}}" style="text-decoration: none">
+        <button class="btn btn-default">Back to articles</button>
+    </a>
     @include('errors.list')
 @stop
